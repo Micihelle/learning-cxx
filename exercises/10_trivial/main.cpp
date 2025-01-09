86d79c16ababebe5d74156df2cfe7e4e7e89be38
micihelle <1158869145@qq.com>
2025-01-09 17:12:50 +0800

    exercise10-11 passed

--- exercises/10_trivial/main.cpp
+++ exercises/10_trivial/main.cpp
@@ -8,19 +8,24 @@ struct FibonacciCache {
 };
 
 // TODO: 实现正确的缓存优化斐波那契计算
+// &cache 表示对 FibonacciCache fib的引用传递（直接对原始缓存进行操作）
 static unsigned long long fibonacci(FibonacciCache &cache, int i) {
-    for (; false; ++cached) {
-        cache[cached] = cache[cached - 1] + cache[cached - 2];
+    for (int cached = 2; cached <= i; ++cached) {
+        cache.cache[cached] = cache.cache[cached - 1] + cache.cache[cached - 2];  
     }
     return cache.cache[i];
 }
 
 int main(int argc, char **argv) {
     // TODO: 初始化缓存结构体，使计算正确
+    // cache[0]=1;cache[1]=1;cache[2]=..;
     // NOTICE: C/C++ 中，读取未初始化的变量（包括结构体变量）是未定义行为
     // READ: 初始化的各种写法 <https://zh.cppreference.com/w/cpp/language/initialization>
-    FibonacciCache fib;
+    FibonacciCache fib={{0,1},2};
+    //std::cout << "fibonacci(10) == " << fibonacci(fib, 10) << std::endl;
+    //std::cout << "fibonacci(9) == " << fibonacci(fib, 9) << std::endl;
     ASSERT(fibonacci(fib, 10) == 55, "fibonacci(10) should be 55");
     std::cout << "fibonacci(10) = " << fibonacci(fib, 10) << std::endl;
     return 0;
 }
+
